b29c28050a561bbca646d25580568d19a4776d4b
Marc <marcramadison006@gmail.com>
2021-04-12 17:36:33 +0300

    Ajouter les contenues de la page services

--- src/Components/serviceBox.js
+++ src/Components/serviceBox.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './ComponentsStyle/serviceBoxCss.css';
 
 const ServiceBox=(props)=>{
   return(
     <div className="ServiceBoxContainer">
-        <img title="" src="" />
-        <div className="serviceDescription">
-
+        <img className="serviceImage" alt="image du service" src={props.serviceImage} />
+        <div className="serviceDescriptionBlock">
+           <h1 className="serviceTitle">
+              {props.serviceTitle}
+           </h1>
+           <p className="serviceDescript">
+              {props.serviceDescription}
+           </p>
         </div>
     </div>
   )
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import Navigateur from '../Components/Navigateur';
-import CompetenceCard from '../Components/CompetenceCard';
+import ServiceBox from '../Components/ServiceBox';
 import Footer from '../Components/Footer';
-import './PageStyle/CompetenceCss.css';
+import './PageStyle/servicesCss.css';
 import './PageStyle/globalCss.css';
 
 const services=[
   {
-    serviceImage:'',
-    serviceTitle:'',
-    serviceDescr:''
+    serviceImage:'./Assets/Images/services/integrateur.jpg',
+    serviceTitle:'Intégrateur web',
+    serviceDescr:'Connaissance parfaite de l\'HTML et du CSS'
+  },
+  {
+    serviceImage:'./Assets/Images/services/frontend.jpg',
+    serviceTitle:'Développement frontend',
+    serviceDescr:'Développer une application frontend avec reactjs'
+  },
+  {
+    serviceImage:'./Assets/Images/services/backend.jpg',
+    serviceTitle:'Développement backend',
+    serviceDescr:'Développer une application backend avec nodejs'
+  },
+  {
+    serviceImage:'./Assets/Images/services/mern.jpg',
+    serviceTitle:'Développement mern stack',
+    serviceDescr:'Développer une application frontend avec reactjs'
   }
+
+
 ]
 
 const Services=()=>{
@@ -22,7 +39,9 @@ const Services=()=>{
                Mes services
             </div>
             <div className="serviceContent">
-
+               {
+                 services.map((service,i)=><ServiceBox key={i} serviceTitle={service.serviceTitle} serviceImage={service.serviceImage} serviceDescription={service.serviceDescr}/>)
+               }
             </div>
        </div>
        <Footer/>
